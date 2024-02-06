798fa1c712984eaccf70435423aab29e6403165c
Himanshu Vishwakarma <vishwakarma.jeetu137@gmail.com>
2024-02-06 23:58:13 +0530

    Validation and Email duplication errors

--- src/errors/Errors.ts
+++ src/errors/Errors.ts
@@ -1,13 +1,23 @@
-interface AuthorError {
-  nameError?: string;
+export interface ErrorResponse {
+  [key: string]: {
+    message: string;
+  };
 }
 
-export const handleErrors = (error: any): AuthorError => {
-  const authorError: AuthorError = {};
-
-  if (error.errors.name) {
-    authorError.nameError = error.errors.name.message;
+export const validationError = (error: any) => {
+  let validationErrorResponse: ErrorResponse = {};
+  for (const field in error.errors) {
+    validationErrorResponse[field] = {
+      message: error.errors[field].message,
+    };
   }
+  return validationErrorResponse;
+};
 
-  return authorError;
+export const duplicateEmailError = (error: any) => {
+  let emailconflictError: ErrorResponse = {};
+  emailconflictError["email"] = {
+    message: "Email already in use.",
+  };
+  return emailconflictError;
 };
