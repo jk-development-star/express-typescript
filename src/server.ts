a31a584079438b70a31840119d1621259d4c170d
Himanshu Vishwakarma <vishwakarma.jeetu137@gmail.com>
2024-02-07 00:01:35 +0530

    Add custom error handler middleware

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
-import express from 'express';
-import http from 'http';
-import mongoose from 'mongoose';
-import { config } from './config/config';
-import Logging from './library/Logging';
-import AuthorRoutes from './routes/Author';
+import express from "express";
+import http from "http";
+import mongoose from "mongoose";
+import { config } from "./config/config";
+import Logging from "./library/Logging";
+import AuthorRoutes from "./routes/Author";
+import { errorHandler } from "./middleware/errorHandler";
+import createHttpError from "http-errors";
 
 const app = express();
 
@@ -11,11 +13,11 @@ const app = express();
 mongoose
   .connect(config.mongo.url, config.mongo.options)
   .then(() => {
-    Logging.info('Connected to MongoDB.');
+    Logging.info("Connected to MongoDB.");
     StartServer();
   })
   .catch((error) => {
-    Logging.error('Unable to connect');
+    Logging.error("Unable to connect");
     Logging.error(error);
   });
 
@@ -26,7 +28,7 @@ const StartServer = () => {
       `Incoming -> Method: [${req.method} - Url: ${req.url} - IP: ${req.socket.remoteAddress}]`
     );
 
-    res.on('finish', () => {
+    res.on("finish", () => {
       Logging.info(
         `Incoming -> Method: [${req.method} - Url: ${req.url} - IP: ${req.socket.remoteAddress}] - Status: ${res.statusCode}`
       );
@@ -37,19 +39,21 @@ const StartServer = () => {
   app.use(express.urlencoded({ extended: true }));
   app.use(express.json());
 
-  app.use('/author', AuthorRoutes);
+  app.use("/authors", AuthorRoutes);
 
   /** Helathcheck */
-  app.get('/test', (req, res, next) =>
-    res.status(200).json({ message: 'Success' })
+  app.get("/test", (req, res, next) =>
+    res.status(200).json({ message: "Success" })
   );
 
   /** Error handling */
   app.use((req, res, next) => {
-    const error = new Error('not found');
+    const error = "Endpoint not found!";
     Logging.error(error);
-    return res.status(404).json({ message: error.message });
+    next(createHttpError(404, error));
   });
+
+  app.use(errorHandler);
 
   http
     .createServer(app)
