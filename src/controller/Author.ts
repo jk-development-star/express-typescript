92921d36c60064b483e9f6ba9e16be9211eca3d9
Himanshu Vishwakarma <vishwakarma.jeetu137@gmail.com>
2024-03-08 12:53:25 +0530

    Add pagination

--- src/controller/Author.ts
+++ src/controller/Author.ts
@@ -1,6 +1,7 @@
 import { RequestHandler } from "express";
 import Author from "../model/Author";
 import { duplicateEmailError, validationError } from "../errors/Errors";
+import createHttpError from "http-errors";
 
 const createAuthor: RequestHandler = async (req, res, next) => {
   try {
@@ -18,9 +19,17 @@ const createAuthor: RequestHandler = async (req, res, next) => {
 };
 
 const getAllAuthors: RequestHandler = async (req, res, next) => {
-  await Author.find({})
-    .then((authors) => res.status(200).json({ authors }))
-    .catch((error) => console.log(error));
+  try {
+    const limit: number = parseInt(req.query.limit as string) || 10;
+    const skip: number = parseInt(req.query.skip as string) || 0;
+    await Author.find()
+      .limit(limit)
+      .skip(skip)
+      .then((authors) => res.status(200).json({ authors }))
+      .catch((error) => res.status(400).json(createHttpError(error.message)));
+  } catch (error) {
+    next(error);
+  }
 };
 
 export { createAuthor, getAllAuthors };
